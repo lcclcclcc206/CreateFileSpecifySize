d4084e6e4622106502f46e82e0d7df54fb915628
lcc206 <1402080954@qq.com>
2022-11-17 12:54:57 +0800

    UI修改，创建项目统一发布文件

--- CreateFile.UI/App.xaml.cs
+++ CreateFile.UI/App.xaml.cs
@@ -13,6 +13,9 @@ namespace CreateFile.UI
     /// </summary>
     public partial class App : Application
     {
-        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
     }
 }
